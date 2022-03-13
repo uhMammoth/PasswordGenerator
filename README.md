36604bd74c4150d01c054bcd6707bd0f330c459e
KyleP <kyl3p3tty@gmail.com>
2022-03-12 20:11:31 -0800

    added user prompt function with condition checks to prevent errors

---
+++ README.md
@@ -0,0 +1,7 @@
+# Password Generator
+Uses javascript to prompt the user then provide a random password for the user.
+
+## Live Link
+
+
+### Made by Kyle Petty
--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,38 @@
+// prompts the user what characters and length they want for the password
+function userPrompt() {
+  var userChoices = {
+    upper: true,
+    lower: true,
+    num: true,
+    special: true,
+    len: 8
+  };
+  userChoices.upper = confirm("Press 'OK' if you want uppercase letters.");
+  userChoices.lower = confirm("Press 'OK' if you want lowercase letters.");
+  userChoices.num = confirm("Press 'OK' if you want numbers.");
+  userChoices.special = confirm("Press 'OK' if you want special characters.");
+  //if user enters number outside range, check will push value to nearest accepted value
+  userChoices.len = prompt("Enter the desired password length (8-128):");
+  if (userChoices.len < 8) {
+    userChoices.len = 8;
+  } else if (userChoices.len > 128){
+    userChoices.len = 128;
+  }
+  
+  //if user declines all characters generator will cancel
+  var userError = 0;
+  for (let i in userChoices){
+    if (!userChoices[i]){
+      userError++;
+    }
+  }
+  if (userError === 4){
+      return userError;
+  } else {
+    return userChoices;
+  }
+}
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
