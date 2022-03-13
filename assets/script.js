156521f4e761a6538a60663f599d63497c00946c
KyleP <kyl3p3tty@gmail.com>
2022-03-12 20:16:06 -0800

    fixed script link not working

--- assets/script.js
+++ assets/script.js
@@ -75,9 +75,7 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
